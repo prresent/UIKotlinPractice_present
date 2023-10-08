db6e89eb5e2c1fc7166bfbb059fdf0f435ce449f
present <present@sookmyung.ac.kr>
2023-10-08 19:18:34 +0900

    xml ID부여 + 코틀린 확장팩 (자동완성 지원)

--- app/src/main/java/com/example/uikotlinpractice_present/MainActivity.kt
+++ app/src/main/java/com/example/uikotlinpractice_present/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.uikotlinpractice_present
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.uikotlinpractice_present.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate((layoutInflater))
+        setContentView(binding.root)
+        binding.contentEdt
+
+        binding.okBtn
+
+        binding.reslutTxt
+
+
     }
 }
